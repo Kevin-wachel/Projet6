4bc7c19e8ab46208c01a7fdb8091732cb74b52e4
Kevin-wachel <kevwachel@gmail.com>
2020-10-02 14:13:43 +0200

    Créer, modifier, supprimer ok.

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 
-const stuffRoutes = require('./routes/stuff');
+const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://Kev:Kev1234@cluster0.ykt2l.mongodb.net/test?retryWrites=true&w=majority',
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-app.use('/api/stuff', stuffRoutes);
+app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
 
 module.exports = app;
